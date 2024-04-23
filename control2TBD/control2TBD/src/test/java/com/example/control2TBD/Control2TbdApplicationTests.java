621f3d1e2f0da6f289931a2cc0e43cd3ab1b2262
Yuyunisse <yuyunisse.carreno@usach.cl>
2024-04-23 15:09:44 -0400

    creacion proyecto

---
+++ control2TBD/control2TBD/src/test/java/com/example/control2TBD/Control2TbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.control2TBD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Control2TbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
