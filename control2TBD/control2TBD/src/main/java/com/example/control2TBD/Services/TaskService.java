dec3fb69574e2ba20439272ded739177f2bea825
Hans Villarroel <hans.villarroel@usach.cl>
2024-04-24 23:17:55 -0400

    Java Controller/Service skeleton

---
+++ control2TBD/control2TBD/src/main/java/com/example/control2TBD/Services/TaskService.java
@@ -0,0 +1,7 @@
+package com.example.control2TBD.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+}
---
+++ control2TBD/control2TBD/src/main/java/com/example/control2TBD/Services/UserService.java
@@ -0,0 +1,8 @@
+package com.example.control2TBD.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
