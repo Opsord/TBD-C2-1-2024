c5d76a2a93cbbf2e865206ee2ce3da8a8e0e4ff4
Hans Villarroel <hans.villarroel@usach.cl>
2024-04-24 23:21:04 -0400

    fixed typo in repositories

--- control2TBD/control2TBD/src/main/java/com/example/control2TBD/Repositories/TaskRepository.java
+++ control2TBD/control2TBD/src/main/java/com/example/control2TBD/Repositories/TaskRepository.java
@@ -3,5 +3,5 @@ package com.example.control2TBD.Repositories;
 import com.example.control2TBD.Entities.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskEntityRepository extends JpaRepository<TaskEntity, Long> {
+public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
 }
--- control2TBD/control2TBD/src/main/java/com/example/control2TBD/Repositories/UserRepository.java
+++ control2TBD/control2TBD/src/main/java/com/example/control2TBD/Repositories/UserRepository.java
@@ -3,5 +3,5 @@ package com.example.control2TBD.Repositories;
 import com.example.control2TBD.Entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 }
